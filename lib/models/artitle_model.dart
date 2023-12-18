e8c2fc8ce8be166f2159796332716616c49bf25b
LiShuxue <1149926505@qq.com>
2023-12-18 11:20:25 +0800

    修改主页，支持列表，滚动和点击

---
+++ lib/models/artitle_model.dart
@@ -0,0 +1,6 @@
+class Article {
+  final int id;
+  final String title;
+
+  Article({required this.id, required this.title});
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:journey_app/models/artitle_model.dart';
 import 'package:journey_app/pages/detail_page.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,13 +11,28 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin {
-  // 页面的状态
-  int _counter = 0;
+  final _scrollController = ScrollController();
+  final List<Article> _articles =
+      List.generate(10, (index) => Article(id: index, title: 'Article $index'));
 
-  // 自定义的修改状态的方法
-  void _addCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadMoreArticles();
+      }
+    });
+  }
+
+  void _loadMoreArticles() {
     setState(() {
-      _counter++;
+      _articles.addAll(List.generate(
+          10,
+          (index) => Article(
+              id: _articles.length + index,
+              title: 'Article ${_articles.length + index}')));
     });
   }
 
@@ -24,31 +40,73 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      body: Center(
-        child: Column(
-          // min 尽可能少的占用主轴方向的空间。当子组件没有占满主轴剩余空间时，Row或Column的实际大小等于所有子组件占用的主轴空间。
-          // max 尽可能多的占用主轴方向的空间。如果子组件没有占满主轴剩余空间，那么剩余的空间会均匀分布在子组件之间。
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('Home Page'),
-            ElevatedButton(
-              onPressed: () {
+      // 滚动
+      body: ListView.builder(
+        controller: _scrollController,
+        itemCount: _articles.length + 1, // 加1是因为第一项是swiper
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            // 第一项是swiper
+            return const Column(
+              children: [
+                SizedBox(
+                  height: 200, // 设置Swiper的高度
+                  child: Text('滑块'),
+                ),
+                SizedBox(height: 10), // 添加10像素的垂直空间
+              ],
+            );
+          } else if (index == _articles.length) {
+            final article = _articles[index - 1];
+            // 最后一项包含加载中
+            return GestureDetector(
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) =>
-                          const DetailPage(id: '1', from: 'home')),
+                          DetailPage(id: '${article.id}', from: 'home')),
                 );
               },
-              child: const Text('Go to Home Detail'),
-            ),
-            Text('$_counter'),
-            ElevatedButton(
-              onPressed: () => _addCounter(),
-              child: const Text('更新状态'),
-            ),
-          ],
-        ),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.image), // 替换为文章图片
+                    title: Text('Article Title ${index - 1}'),
+                    subtitle: Text('Article Subtitle ${index - 1}'),
+                  ),
+                  const Divider(),
+                  const CircularProgressIndicator(),
+                  const SizedBox(height: 10),
+                  const Text('加载中...'),
+                ],
+              ),
+            );
+          } else {
+            // 其他项正常展示
+            final article = _articles[index - 1]; // 获取文章
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          DetailPage(id: '${article.id}', from: 'home')),
+                );
+              },
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.image), // 替换为文章图片
+                    title: Text('Article Title ${index - 1}'),
+                    subtitle: Text('Article Subtitle ${index - 1}'),
+                  ),
+                  const Divider(),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
