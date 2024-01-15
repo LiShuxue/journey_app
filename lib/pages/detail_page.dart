966a1ca479a36ed0c2f598ac8fe38d1930dfd528
LiShuxue <1149926505@qq.com>
2024-01-15 21:02:16 +0800

    使用flutter_markdown显示文章详情

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,31 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
-class DetailPage extends StatelessWidget {
+import 'package:journey_app/utils/dio_config.dart';
+
+class DetailPage extends StatefulWidget {
   const DetailPage({required this.id, super.key});
 
   // 参数：获取id
   final String id;
 
   @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  // 博客详情
+  dynamic _blogDetail;
+
+  Future<dynamic> getDetailById(String id) async {
+    Response response = await dio.get('/blog-api/blog/detail?id=$id');
+    dynamic data = response.data['blog'];
+    setState(() {
+      _blogDetail = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDetailById(widget.id);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Detail Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Details for id: $id'),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go Back'),
+    if (_blogDetail == null) {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Detail'),
+          ),
+          body: const Center(
+            child: CircularProgressIndicator(),
+          ));
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(_blogDetail['title']),
+        ),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+                padding: const EdgeInsets.all(10),
+                child: Image.network(_blogDetail['image']['url'])),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 15, left: 10, right: 10, bottom: 15),
+              child: MarkdownBody(data: _blogDetail['markdownContent']),
             ),
           ],
         ),
-      ),
-    );
+      );
+    }
   }
 }
